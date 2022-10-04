c083c4ec8a9aa379846d6abf50cba4cd9baf62d7
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-10-03 19:33:09 -0500

    Finished studio

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -2,7 +2,11 @@ package org.launchcode.spaday.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.ArrayList;
 
 
@@ -12,22 +16,19 @@ public class SpaDayController {
     public boolean checkSkinType(String skinType, String facialType) {
         if (skinType.equals("oily")) {
             return facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating");
-        }
-        else if (skinType.equals("combination")) {
+        } else if (skinType.equals("combination")) {
             return facialType.equals("Microdermabrasion") || facialType.equals("Rejuvenating") || facialType.equals("Enzyme Peel");
-        }
-        else if (skinType.equals("dry")) {
+        } else if (skinType.equals("dry")) {
             return facialType.equals("Rejuvenating") || facialType.equals("Hydrofacial");
-        }
-        else {
+        } else {
             return true;
         }
     }
 
-    @GetMapping(value="")
+    @GetMapping(value = "")
     @ResponseBody
-    public String customerForm () {
-        String html = "<form method = 'post'>" +
+    public String customerForm() {
+        String html = "<form method = 'post' action='spaMenu'>" +
                 "Name: <br>" +
                 "<input type = 'text' name = 'name'>" +
                 "<br>Skin type: <br>" +
@@ -41,14 +42,19 @@ public class SpaDayController {
                 "<select name = 'manipedi'>" +
                 "<option value = 'manicure'>Manicure</option>" +
                 "<option value = 'pedicure'>Pedicure</option>" +
+                "<option value = 'both'>Manipedi</option>" +
                 "</select><br>" +
                 "<input type = 'submit' value = 'Submit'>" +
                 "</form>";
         return html;
     }
 
-    @PostMapping(value="")
+    @PostMapping(value = "spaMenu")
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
+
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
 
         ArrayList<String> facials = new ArrayList<>();
         facials.add("Microdermabrasion");
@@ -57,11 +63,12 @@ public class SpaDayController {
         facials.add("Enzyme Peel");
 
         ArrayList<String> appropriateFacials = new ArrayList<>();
-        for (int i = 0; i < facials.size(); i ++) {
-            if (checkSkinType(skintype,facials.get(i))) {
+        for (int i = 0; i < facials.size(); i++) {
+            if (checkSkinType(skintype, facials.get(i))) {
                 appropriateFacials.add(facials.get(i));
             }
         }
+        model.addAttribute("appropriateFacials", appropriateFacials);
 
         return "menu";
     }
